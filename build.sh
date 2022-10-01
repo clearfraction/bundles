f8e61929a9a5e4ad450a47dfe1f9313ed729a192
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2022-10-01 15:37:23 +0300

    do not update version before format bump

--- build.sh
+++ build.sh
@@ -85,7 +85,7 @@ popd
 
 # Add bundles to the mix
 mixer bundle add `ls /mixer/local-bundles`
-mixer versions update --mix-version $RELEASE --upstream-version $RELEASE
+# mixer versions update --mix-version $RELEASE --upstream-version $RELEASE
 
 # Format bump
 mixer build upstream-format --new-format 31
