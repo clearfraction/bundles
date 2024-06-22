30ecd05f043f26338a9cef351b30b0ca5409f64a
K1ngfish3r <trulyihu@gmail.com>
2024-06-22 17:14:43 +0800

    init and uninstall scripts

--- cf-waydroid-preinstall.sh
+++ cf-waydroid-preinstall.sh
@@ -19,7 +19,7 @@ kernel_params=(
     "psi=1"
 )
 
-# Function to check if line already exists
+# Function to check if a line already exists
 line_checker() {
     for conf in "$cmdline_dir"/*.conf; do
         if [[ -f "$conf" ]] && grep -qFx "$1" "$conf"; then
@@ -41,5 +41,37 @@ sudo clr-boot-manager update
 
 # Enable libvirtd and additional services
 sudo systemctl enable libvirtd libvirt-guests virtlxcd virtstoraged virtnetworkd virtnodedevd
+
+# Array for creating symlinks, change if needed
+files=(
+    "/lib/systemd/system/lxc-monitord.service"
+    "/lib/systemd/system/lxc-net.service"
+    "/lib/systemd/system/lxc.service"
+    "/lib/systemd/system/lxc@.service"
+    "/lib/systemd/system/waydroid-container.service"
+    "/lib/tmpfiles.d/lxc.conf"
+    "/libexec/lxc/hooks/unmount-namespace"
+    "/libexec/lxc/lxc-apparmor-load"
+    "/libexec/lxc/lxc-containers"
+    "/libexec/lxc/lxc-monitord"
+    "/libexec/lxc/lxc-net"
+    "/libexec/lxc/lxc-user-nic"
+    "/sbin/init.lxc"
+    "/share/dbus-1/system-services/id.waydro.Container.service"
+    "/share/dbus-1/system.d/id.waydro.Container.conf"
+    "/share/polkit-1/actions"
+    "/share/lxc"
+)
+
+# Iterate over array
+source_dir="/opt/3rd-party/bundles/clearfraction/usr"
+target_dir="/usr"
+for file in "${files[@]}"; do
+    if [ -e "$source_dir$file" ]; then
+        ln -sfn "$source_dir$file" "$target_dir$file"
+    else
+        echo "File $source_dir$file does not exist."
+    fi
+done
 
 echo "Setup complete. Restart for the bootloader configuration to take effect"
